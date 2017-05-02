e0deda3c96ae5f6ea7c33417e0aabc8e4831bc2d
Harsh <pharsh22@vt.edu>
2017-05-02 08:47:42 -0400

    Minor change in .m files

--- scatterPlotOfInput.m
+++ scatterPlotOfInput.m
@@ -5,7 +5,7 @@ close all;
 % x = [1, 2 ,2 ,2 ,4 ,4 ,4 ,5 ,5 ,5 ,6 ,6 ,7 ,8 ,9 ,10 ,11 ,12]-0.5;
 % y = [1 1 2 3 1 2 3 2 3 4 2 3 2 5 6 4 7 7]-0.5;
 
-fileID = fopen('input2.txt', 'r');
+fileID = fopen('input.txt', 'r');
 formatSpec = '%f %f';
 sizeA = [2 inf];
 
@@ -16,6 +16,8 @@ A = A-0.5;
 
 figure;
 scatter(A(:,1),A(:,2), 'filled');
+xlabel('x');
+ylabel('y');
 tickValues = 0:1:12;
 set(gca, 'XTick', tickValues);
 set(gca, 'YTick', tickValues);
--- visualizeMWT.m
+++ visualizeMWT.m
@@ -2,7 +2,30 @@ clc;
 clear all;
 close all;
 
-fileID = fopen('mwtOutput2.txt', 'r');
+fileID = fopen('input.txt', 'r');
+formatSpec = '%f %f';
+sizeA = [2 inf];
+
+A = fscanf(fileID,formatSpec,sizeA);
+A = A';
+[m,n] = size(A);
+A = A-0.5;
+
+figure;
+hold on;
+scatter(A(:,1),A(:,2), 'filled');
+xlabel('x');
+ylabel('y');
+tickValues = 0:1:12;
+set(gca, 'XTick', tickValues);
+set(gca, 'YTick', tickValues);
+grid on;
+axis([0 12 0 10]);
+
+
+
+
+fileID = fopen('mwtOutputSorted.txt', 'r');
 formatSpec = '%f %f %f %f';
 sizeA = [4 inf];
 
@@ -10,8 +33,6 @@ A = fscanf(fileID,formatSpec,sizeA);
 A = A';
 [m,n] = size(A);
 
-
-figure;
 for i=1:m
     x = [A(i,1) A(i,3)];
     y = [A(i,2) A(i,4)];
